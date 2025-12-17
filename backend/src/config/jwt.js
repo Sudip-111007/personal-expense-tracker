f06d247965d487b5069579b5c9f3725148d57638
CYBERPUNK <sudipmaji529@gmail.com>
2025-12-17 20:24:27 +0530

    Delete backend directory

--- backend/src/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  expiresIn: "7d"
-};
--- backend/src/routes/analysisRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { analyzeExpenses } = require("../controllers/analysisController");
-const protect = require("../middleware/authMiddleware");
-
-router.post("/analyze", protect, analyzeExpenses);
-
-module.exports = router;
